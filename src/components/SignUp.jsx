bdcc960dd83fe055264ac5a80d3384c750dc5df1
Prashant2411 <Prashant241197@gmail.com>
2020-05-17 13:19:32 +0530

    Added: Validation to Register Page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
 import "../css/SignUp.css";
+import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import IconButton from "@material-ui/core/IconButton";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import regex from "../json/Regex.json";
+import DoneIcon from "@material-ui/icons/Done";
+import ClearIcon from "@material-ui/icons/ErrorOutlineSharp";
 
 const styles = (theme) => ({
   container: {
-    margin: "8% 20% 0 20%",
+    margin: "4% 20% 0 20%",
     border: "solid 1px",
     padding: "2% 5%",
     backgroundColor: "white",
@@ -25,11 +29,26 @@ const styles = (theme) => ({
   textField: {
     width: "100%",
   },
+  doneIcon: {
+    color: "green",
+  },
+  notDoneIcon: {
+    color: "red",
+  },
 });
 
 class SignUp extends React.Component {
   state = {
     showPassword: false,
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    cpassword: "",
+    mobileNumber: "",
+    age: "",
+    errorField: [],
+    emailValid: "",
   };
 
   showPassword = () => {
@@ -38,11 +57,15 @@ class SignUp extends React.Component {
       : this.setState({ showPassword: true });
   };
 
+  handleChange = async (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
     const { classes } = this.props;
 
     return (
-      <div className={classes.container}>
+      <form className={classes.container}>
         <Grid
           container
           direction="column"
@@ -67,28 +90,58 @@ class SignUp extends React.Component {
                 className={classes.textField}
                 name="firstName"
                 margin="normal"
+                inputProps={{
+                  pattern: regex.firstName,
+                  title: "Alphabets Only",
+                }}
                 required
+                onChange={this.handleChange}
                 variant="outlined"
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
+                error={this.state.lastNameE}
                 label="Last Name"
                 type="text"
                 name="lastName"
                 className={classes.textField}
+                InputProps={{
+                  inputProps: {
+                    pattern: regex.lastName,
+                    title: "Alphabets Only",
+                  },
+                }}
                 margin="normal"
                 variant="outlined"
+                onChange={this.handleChange}
                 required
               />
             </Grid>
             <Grid item xs={12} sm={12}>
               <TextField
+                error={this.state.emailE}
                 label="Email ID"
                 name="email"
                 className={classes.textField}
                 margin="normal"
+                inputProps={{
+                  pattern: regex.email,
+                  title: "Enter email in format example@example.com",
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <React.Fragment>
+                      {this.state.emailValid === "YES" ? (
+                        <DoneIcon className={classes.doneIcon} />
+                      ) : this.state.emailValid === "NO" ? (
+                        <ClearIcon className={classes.notDoneIcon} />
+                      ) : null}
+                    </React.Fragment>
+                  ),
+                }}
                 variant="outlined"
+                onChange={this.handleChange}
                 required
               />
             </Grid>
@@ -98,9 +151,17 @@ class SignUp extends React.Component {
                 className={classes.textField}
                 name="password"
                 type={this.state.showPassword ? "text" : "password"}
+                InputProps={{
+                  inputProps: {
+                    pattern: regex.password,
+                    title:
+                      "Must Contain 1 capital and small letter, 1 digit and 1 special character and length between 6 to 15 ",
+                  },
+                }}
                 variant="outlined"
                 margin="normal"
-                onChange={this.props.handleChange}
+                onChange={this.handleChange}
+                required
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -113,49 +174,74 @@ class SignUp extends React.Component {
                 margin="normal"
                 InputProps={{
                   endAdornment: (
-                    <InputAdornment>
-                      <IconButton
-                        className={classes.iconButton}
-                        onClick={this.showPassword}
-                      >
-                        {this.state.showPassword ? (
-                          <VisibilityOffIcon />
-                        ) : (
-                          <VisibilityIcon />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
+                    <IconButton
+                      className={classes.iconButton}
+                      onClick={this.showPassword}
+                    >
+                      {this.state.showPassword ? (
+                        <VisibilityOffIcon />
+                      ) : (
+                        <VisibilityIcon />
+                      )}
+                    </IconButton>
                   ),
+                  inputProps: {
+                    pattern: this.state.password,
+                    title: "Passwords does not match",
+                  },
                 }}
-                onChange={this.props.handleChange}
+                onChange={this.handleChange}
+                required
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
+                error={this.state.mobileNumberE}
                 label="Mobile Number"
                 name="mobileNumber"
-                type="number"
+                onChange={this.handleChange}
                 className={classes.textField}
                 InputProps={{
                   startAdornment: <InputAdornment>+91-</InputAdornment>,
                 }}
+                inputProps={{
+                  pattern: regex.mobileNumber,
+                  title: "Enter valid phone number",
+                }}
                 variant="outlined"
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
+                error={this.state.ageE}
                 label="Age"
                 name="age"
                 type="number"
                 className={classes.textField}
+                InputProps={{
+                  inputProps: {
+                    max: 150,
+                    min: 1,
+                  },
+                }}
                 margin="normal"
                 variant="outlined"
+                onChange={this.handleChange}
                 required
               />
             </Grid>
           </Grid>
+          <Button
+            variant="contained"
+            color="primary"
+            name="currentInput"
+            type="submit"
+            className={classes.button}
+          >
+            SIGN IN
+          </Button>
         </Grid>
-      </div>
+      </form>
     );
   }
 }
